7929e3c591b65f60f48c0662ba1d0447c86e8fe3
Manoel do Nascimento <manu3105@yahoo.com>
2020-08-13 18:36:14 -0300

    todo props rendered

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Todos from './components/Todos';
+
 import './App.css';
 
-function App() {
-  return (
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>App</h1>
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Clean bedroom',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Wash dishes',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Brush teeth',
+        completed: true
+      }
+    ]
+  }
+
+  render() {
+    return(
     <div className="App">
-      <h1>ToDo List</h1>
+      {/* <h1>Apps</h1> */}
+      <Todos todos={this.state.todos}/>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
