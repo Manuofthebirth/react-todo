a2082ba3dbc6b65cc567e87e3b205ff4a3dd9e6d
Manoel do Nascimento <manu3105@yahoo.com>
2020-08-13 20:39:13 -0300

    Inline and variable style added

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,7 +4,8 @@ import PropTypes from 'prop-types';
 export class TodoItem extends Component {
   render() {
     return (
-      <div>
+      // <div style={{color: '#e33', backgroundColor: '#f4f4f4'}}>
+      <div style={itemStyle}>  
         <h3>{this.props.todo.title}</h3>
       </div>
     )
@@ -15,5 +16,12 @@ export class TodoItem extends Component {
 TodoItem.propTypes = {
   todo: PropTypes.object.isRequired
 }
+
+// style variable
+const itemStyle = {
+  color: '#e33', 
+  backgroundColor: '#f4f4f4'
+}
+
 
 export default TodoItem
