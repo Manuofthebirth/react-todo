4e394b442fa1f5b687316fe0b9af82394699afad
Manoel do Nascimento <manu3105@yahoo.com>
2020-08-13 21:31:33 -0300

    Ternary operator style added

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,18 +2,29 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types';
 
 export class TodoItem extends Component {
-  // dynamic style
+  // dynamic style - ternary operator 
   getStyle = () => {
-    if (this.props.todo.completed) {
-      return {
-        textDecoration: 'line-through'      
-      }
-    } else {
-      return {
-        textDecoration: 'none'
-      }
+    return {
+      color: '#e33', 
+      backgroundColor: '#f4f4f4',
+      padding: '10px',
+      borderBottom: '1px #000 solid',
+      textDecoration: this.props.todo.completed ? 'line-through' : 'none'
     }
-  }  
+  }
+  
+  // dynamic style
+  // getStyle = () => {
+  //   if (this.props.todo.completed) {
+  //     return {
+  //       textDecoration: 'line-through'      
+  //     }
+  //   } else {
+  //     return {
+  //       textDecoration: 'none'
+  //     }
+  //   }
+  // }  
 
   render() {
     return (
