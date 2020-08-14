addf8ac66410c55a55c5a0f0e0a1eb578b2199c2
Manoel do Nascimento <manu3105@yahoo.com>
2020-08-13 21:17:49 -0300

    Dynamic style added

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,10 +2,24 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types';
 
 export class TodoItem extends Component {
+  // dynamic style
+  getStyle = () => {
+    if (this.props.todo.completed) {
+      return {
+        textDecoration: 'line-through'      
+      }
+    } else {
+      return {
+        textDecoration: 'none'
+      }
+    }
+  }  
+
   render() {
     return (
       // <div style={{color: '#e33', backgroundColor: '#f4f4f4'}}>
-      <div style={itemStyle}>  
+      // <div style={itemStyle}>  
+      <div style={this.getStyle()}>
         <h3>{this.props.todo.title}</h3>
       </div>
     )
@@ -18,10 +32,10 @@ TodoItem.propTypes = {
 }
 
 // style variable
-const itemStyle = {
-  color: '#e33', 
-  backgroundColor: '#f4f4f4'
-}
+// const itemStyle = {
+//   color: '#e33', 
+//   backgroundColor: '#f4f4f4'
+// }
 
 
 export default TodoItem
