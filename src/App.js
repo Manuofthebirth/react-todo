fa0cb33d20f008a6cd03c7aea1476abf3ec81f78
Manoel do Nascimento <manu3105@yahoo.com>
2020-08-17 17:54:55 -0300

    ToDo items with genrated random ids

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
+import Header from './components/layout/Header';
+import AddTodo from './components/AddTodo';
 import Todos from './components/Todos';
+import { v4 as uuidv4 } from 'uuid';
 
 import './App.css';
 
@@ -15,17 +18,17 @@ class App extends Component {
   state = {
     todos: [
       {
-        id: 1,
+        id: uuidv4(),
         title: 'Clean bedroom',
         completed: false
       },
       {
-        id: 2,
+        id: uuidv4(),
         title: 'Wash dishes',
         completed: false
       },
       {
-        id: 3,
+        id: uuidv4(),
         title: 'Brush teeth',
         completed: false
       }
@@ -43,20 +46,37 @@ class App extends Component {
     })}); 
   }
 
-  // Delete
+  // Delete Todo
   // copy everything in array >>> ... (spread operator) ; return id =s which are not equal to the id passed in "deleteTodo = (id) =>"
   deleteTodo = (id) => {
     this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] });
+  }
+  
+  // Add Todo
+  addTodo = (title) => {
+    const newTodo = {
+      id: uuidv4(),
+      // title: title,
+      title,
+      completed: false
+    }
+    // creates new Todo and add it to state
+    this.setState({ todos: [...this.state.todos, newTodo] });
   }
 
   render() {
     return(
     <div className="App">
+      <div className="container">
+      <Header />
+      <AddTodo addTodo={this.addTodo} />
+
       {/* <h1>Apps</h1> */}
       {/* <Todos todos={this.state.todos} /> */}
       
       {/* state is in App.js ;  */}
       <Todos todos={this.state.todos} toggleComplete={this.toggleComplete} deleteTodo={this.deleteTodo} />
+      </div>
     </div>
     )
   }
