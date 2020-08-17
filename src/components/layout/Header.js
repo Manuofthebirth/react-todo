57a787c16a39977aa5df4efdcc11371e57a84978
Manoel do Nascimento <manu3105@yahoo.com>
2020-08-17 15:31:01 -0300

    Header style added

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -2,10 +2,18 @@ import React from 'react';
 
 function Header() {
   return (
-    <header>
+    <header style={headerStyle}>
       <h1>ToDo List</h1>
     </header>
   )
+}
+
+// Header style variable
+const headerStyle = {
+  backgroundColor: '#000',
+  color: '#fff',
+  padding: '10px',
+  textAlign: 'center'
 }
 
 export default Header;
