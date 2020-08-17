cc719864addf52c65664163e26cfabba75e5d0cd
Manoel do Nascimento <manu3105@yahoo.com>
2020-08-17 17:36:01 -0300

    Add new ToDo functionality added

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react'
 
 export class AddTodo extends Component {
+  state = {title: ''}
+  
+  onSubmit = (event) => {
+    // prevent from submiting to file
+    event.preventDefault();
+    // pass title then reset it
+    this.props.addTodo(this.state.title);
+    this.setState({title: ''});
+  }
+  // gives a value to title when typing
+  // onChange = (event) => this.setState({title: event.target.value});
+  onChange = (event) => this.setState({[event.target.name]: event.target.value});
+
+
   render() {
     return (
-      <form style={{display: 'flex'}}>
+      <form onSubmit={this.onSubmit} style={{display: 'flex'}}>
         <input 
           type="text" 
           name="title" 
           style={{flex:'10', padding: '5px'}}
-          placeholder="Add Todo"
+          placeholder="Add Todo.."
+          value={this.state.title}
+          onChange={this.onChange}
         />
         <input 
           type="submit"
